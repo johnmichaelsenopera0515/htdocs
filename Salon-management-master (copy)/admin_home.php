497ace61b2159417065314497d40a4edbb0de12e
johnmichaelsenopera0518 <johnmichael.senopera301420@gmail.com>
2024-04-21 20:11:16 -0700

    ORIGINAL

---
+++ Salon-management-master (copy)/admin_home.php
@@ -0,0 +1,31 @@
+<?php
+include 'admin_header.php';
+	echo "<center><h1>Welcome to Admin</h1>";
+?>
+	     	<div class="clear"></div>
+	      </div>	     
+	  </div>	
+	  </div>
+	   
+       <div class="slider">
+       	 <div class="wrap">
+       	   <div class="slider_top">         
+         		<div class="slider_left">
+				  <div class="wmuSlider example2">
+					<div class="wmuSliderWrapper">
+					<article> <img src="images/SERVEE-FB-BANNER-2.jpg" alt="" /> </article>
+					</div>	
+					<script src="js/jquery.wmuSlider.js"></script> 
+								
+				 </div>
+				</div>				
+				   <div class="slider_right">
+				      <img src="images/face.jpg" alt="" style="    height: 420px; width: 500px;"/>
+				         <div class="sliderright-text">
+				  
+				       </div>
+				   </div>
+				<?php
+include 'footer.php';
+				?>
+				
---
+++ Salon-management-master (copy)/footer.php
@@ -0,0 +1,74 @@
+        
+			   </div>
+		   </div>
+    	     
+	      
+				
+			   </div>
+		  </div>
+      
+ 
+				
+	   <div class="clear"></div>
+     </div>
+    </div>
+  </div>
+  <div class="footer-strip"> </div>
+ <div class="footer">
+   	  <div class="wrap">
+   	    <div class="footer_grides">
+   	    	<div class="footer_grid1">
+					<h3>Information</h3>
+								<ul>
+						            <li><a href="#">About Us</a></li>
+						     		<li><a href="#">Privacy Policy</a></li>
+						     		<li><a href="#">Newsletter</a></li>
+						     		<li><a href="#">Site Map</a></li>						     		
+						   	   </ul>	
+						
+					  	</div>
+				<div class="footer_grid2">
+					<h3>Get In Touch</h3>
+							<div class="address">
+							<ul>
+						  	 <li>Reflection Salon & Spa,</li>
+						  	  <li>New City,</li>
+						  	   <li>Philippines.</li>
+						  	 <li>www.reflection@gmail.com</li>
+						  	 <li><span>Telephone :</span> +06821458963</li>
+						  	 <li><span>Fax :</span> +01478523690</li>
+						  </ul>
+				   </div>				  
+			     </div>
+				<div class="footer_grid3">
+					<h3>Our Company</h3>
+						<div class="f_menu">
+							   <ul>
+						            <li><a href="#">About your Company</a></li>
+						     		<li><a href="#">Terms &amp; conditions</a></li>
+						     		<li><a href="#">News</a></li>
+						     		<li><a href="#">Team of professionals</a></li>	
+						     		<li><a href="#">Testimonials</a></li>					     		
+						   	   </ul>
+						</div>
+				   </div>				
+		  <div class="footer_grid4">
+			<h3>Follow US</h3>
+				<div class="img_list">
+				    <ul>
+					<li><img src="images/28.png" alt=""><a href="https://www.facebook.com/profile.php?id=61557014187880&mibextid=opq0tG/"> JOIN US ON FACEBOOK</a></p>
+					     <li><img src="images/twitter.png" alt=""><a href="#">Share Us on Twitter</a></li>
+					     <li><img src="images/39.png" alt=""><a href="mailto:servee645@gmail.com"> SHARE US ON GMAIL</a></p>
+				    </ul>
+				</div>
+		 </div>
+	   <div class="clear"></div>
+     </div>
+    </div>
+  </div>
+<div class="copy_right">
+				<p>© All rights Reserved | 2024 Brought To You By <a href="https://www.facebook.com/profile.php?id=61557014187880&mibextid=opq0tG/"> Servee PH</a></p>
+		 </div>
+</body>
+</html>
+
